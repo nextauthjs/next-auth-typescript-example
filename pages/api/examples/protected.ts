a614b564fbd22240fc19d8d08680fe83de3a22e0
ndom91 <yo@ndo.dev>
2021-08-11 22:22:05 +0200

    feat: migrate to v4.0.0-next.20

--- pages/api/examples/protected.ts
+++ pages/api/examples/protected.ts
@@ -1,5 +1,5 @@
 // This is an example of to protect an API route
-import { getSession } from "next-auth/client"
+import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 export default async function protectedHandler(
